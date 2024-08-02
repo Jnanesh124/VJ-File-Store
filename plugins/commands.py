492d05bfd29277e800f7f0d4ca0b11a2b8387146
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-08-02 18:53:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,10 +13,12 @@ from plugins.dbusers import db
 from pyrogram import Client, filters, enums
 from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
-from pyrogram.errors import ChatAdminRequired, FloodWait
+from pyrogram.errors import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import *
 from utils import verify_user, check_token, check_verification, get_token
 from config import *
+from info import AUTH_CHANNEL
 import re
 import json
 import base64
@@ -26,6 +28,18 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
+async def is_subscribed(bot, query, channel):
+    btn = []
+    for id in channel:
+        chat = await bot.get_chat(int(id))
+        try:
+            await bot.get_chat_member(id, query.from_user.id)
+        except UserNotParticipant:
+            btn.append([InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)])
+        except Exception as e:
+            pass
+    return btn
+    
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
@@ -49,7 +63,6 @@ def get_size(size):
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
-    username = (await client.get_me()).username
     if AUTH_CHANNEL:
         try:
             btn = await is_subscribed(client, message, AUTH_CHANNEL)
