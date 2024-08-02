c7cbad19077f7e468ae46b674fa06ed73a5b7365
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2024-08-02 18:06:06 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -361,19 +361,6 @@ async def not_joined(client: Client, message: Message):
         )
     except IndexError:
         pass
-
-    await message.reply(
-        text = FORCE_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-        reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
-
         
 @Client.on_message(filters.command('api') & filters.private)
 async def shortener_api_handler(client, m: Message):
